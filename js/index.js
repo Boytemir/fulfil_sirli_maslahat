066d6037774e225941939448ec80eb25ded7a880
Boytemir <boytemirxayitmurodov@gmail.com>
2023-08-22 13:44:08 +0500

    pdf

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 let form__box = document.querySelector(".form__box"),
     close = document.querySelector(".close"),
     btn = document.querySelector(".btn"),
-    form_card = document.querySelector(".form-card");
-
+    form_card = document.querySelector(".form-card"),
+    form__btn = document.querySelector(".form__btn");
 
 // add qo'llanma
 btn.addEventListener("click", () => {
@@ -21,4 +21,21 @@ close.addEventListener("click", () => {
     form__box.classList.remove("actve");
 });
 
+
+// input pustoy net
+
+let ism = document.querySelector("#ism"),
+    tel = document.querySelector("#tel");
+
+
+
+form__btn.addEventListener("click", () => {
+    if(ism.value.length > 0 && tel.value.length > 0) {
+        form__box.classList.remove("actve");
+
+        form__btn.href = "pdf/Image to PDF 20230702 10.11.50.pdf";
+        form__btn.target = "_blank";
+
+    }
+});
 
